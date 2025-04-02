6094509727915ab3690bd358779192a9bbad1de8
gayabarumalamselatan <soerjahartono22@gmail.com>
2025-04-02 12:43:56 +0700

    updating vercel config

--- index.js
+++ index.js
@@ -10,7 +10,7 @@ const app = express();
 
 app.use(express.json());
 app.use(cors());
-const port = process.env.PORT;
+const port = process.env.PORT || 3000;
 
 app.get('/', (req, res) => {
   return res.status(200).json({
