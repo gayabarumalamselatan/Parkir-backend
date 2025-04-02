1a65ddc27fb76be8b72dfcb0dc2992864b444517
gayabarumalamselatan <soerjahartono22@gmail.com>
2025-04-02 12:26:30 +0700

    fixing

--- index.js
+++ index.js
@@ -12,7 +12,7 @@ app.use(express.json());
 app.use(cors());
 const port = process.env.PORT;
 
-app.use('/', (req, res) => {
+app.get('/', (req, res) => {
   return res.status(200).json({
     message: "Server is running",
   })
